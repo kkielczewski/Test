6e8e75284c67a23135b457e80a367a6f51f78c7c
kkielczewski <kam.kielczewski@gmail.com>
2018-08-16 20:04:36 +0200

    test

--- client/source/containers/videos.jsx
+++ client/source/containers/videos.jsx
@@ -30,7 +30,6 @@ class Videos extends React.Component {
     return (
       <div className='videoFeed' >
         {this.state.currentVideos.map(video => <VideoPanel video={video} />)}
-        <div style={{ margin: 'auto', display: 'block' }} >
           <Pagination
           hideDisabled
           hideFirstLastPages
@@ -46,7 +45,6 @@ class Videos extends React.Component {
           nextPageText='>'
           onChange={this.handlePageChange}
           />
-        </div>
       </div>
     );
   }
