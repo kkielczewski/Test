6c2631eb6f34f581ea49531365c803a090405638
Unknown <kam.kielczewski@gmail.com>
2019-02-15 13:48:33 +0100

    video play button

--- client/source/app/components/video-card-big.jsx
+++ client/source/app/components/video-card-big.jsx
@@ -5,7 +5,9 @@ class VideoCardBig extends React.Component {
   render() {
     return (
       <Card>
-        <a draggable='false' href={`//youtube.com/watch?v=${this.props.id}`} />
+        <a draggable='false' href={`//youtube.com/watch?v=${this.props.id}`} >
+          <i class="far fa-play-circle play-button"></i>
+        </a>
         <Image src={this.props.image} />
       </Card>
     );
